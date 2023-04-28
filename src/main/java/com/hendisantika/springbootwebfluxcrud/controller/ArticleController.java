c0fd88cd8ea4bcfc0a9c8ef64eb564516087c6a4
hendisantika <hendisantika@yahoo.co.id>
2023-04-29 03:06:04 +0700

    #15 - Add createArticle in ArticleController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootwebfluxcrud/controller/ArticleController.java
+++ src/main/java/com/hendisantika/springbootwebfluxcrud/controller/ArticleController.java
@@ -3,10 +3,11 @@ package com.hendisantika.springbootwebfluxcrud.controller;
 import com.hendisantika.springbootwebfluxcrud.entity.Article;
 import com.hendisantika.springbootwebfluxcrud.service.ArticleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +29,11 @@ public class ArticleController {
     @GetMapping("/findAll")
     public Flux<Article> getAllArticles() {
         return articleService.findAllArticles();
+    }
+
+    @PostMapping("/save")
+    public Mono<ResponseEntity<Article>> createArticle(@RequestBody Article article) {
+        return articleService.saveArticle(article)
+                .map(savedArticle -> new ResponseEntity<>(savedArticle, HttpStatus.CREATED));
     }
 }
