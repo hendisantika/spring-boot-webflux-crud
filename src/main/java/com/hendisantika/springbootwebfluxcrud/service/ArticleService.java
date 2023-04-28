5dbe3cf20bcc3edf06dfa1c0b9b1f8d418fae7e3
hendisantika <hendisantika@yahoo.co.id>
2023-04-29 03:03:11 +0700

    #6 - Add ArticleService ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootwebfluxcrud/service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootwebfluxcrud.service;
+
+import com.hendisantika.springbootwebfluxcrud.entity.Article;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/29/23
+ * Time: 03:02
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ArticleService {
+
+    Mono<Article> saveArticle(Article article);
+
+    Flux<Article> findAllArticles();
+
+    Mono<Article> findOneArticle(Integer id);
+
+    Flux<Article> findByAuthor(String author);
+
+    Mono<Void> deleteArticle(Integer id);
+}
