6d7f6c118ef95e34aa7ddb54f6705696235d8628
hendisantika <hendisantika@yahoo.co.id>
2023-04-29 02:48:09 +0700

    #1 - Initial commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootwebfluxcrud/SpringBootWebfluxCrudApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebfluxcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootwebfluxcrud/SpringBootWebfluxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebfluxcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
